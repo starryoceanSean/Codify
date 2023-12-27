eecb3bfcbf28b27e738bbf3572a248b06a3c9188
Sean S2 <140343656+starryoceanSean@users.noreply.github.com>
2023-12-27 20:44:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,20 +5,22 @@ const axios      = require('axios'),
       {}         = require('./config'),
       path       = require('path'),
       serverless = require('serverless-http'),
-      port       = 3000;
+      port       = process.env.PORT || 1337;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-  console.log(`[ READY ] › Servidor rodando na porta ${port}.`);
-});
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('body-parser');
 
 app.get('/', (req, res) => {
   res.render('index', {
     // style: path.join(__dirname, '/src/css/style.css')
   });
+});
+
+app.listen(port, () => {
+  console.log(`[ READY ] › Servidor rodando na porta ${port}.`);
 });
 
 module.exports = app;
